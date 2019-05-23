76a722cc369d79617eaa7f9f68d4f2543ad2c2f9
Chris Drackett <chrisdrackett@users.noreply.github.com>
2019-05-23 08:06:05 -0700

    remove extra stuff in babel config

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,3 @@
-module.exports = api => {
-  api.cache(true);
-
-  const presets = ["babel-preset-expo"];
-
-  const plugins = [
-    [
-      "module-resolver",
-      {
-        root: ["./"],
-        alias: {
-          "^react-native$": "react-native-web",
-          ui: "@test/ui"
-        }
-      }
-    ]
-  ];
-
-  return {
-    presets,
-    plugins
-  };
+module.exports = {
+  presets: ["babel-preset-expo"]
 };
