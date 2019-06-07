9b2756e18d54f93302a972c1d9fa05901df1ac97
Chris Drackett <chrisdrackett@users.noreply.github.com>
2019-06-07 12:59:45 -0700

    using a project created by expo-cli

---
+++ web-build/precache-manifest.e2af5b962364748f116e8262dadc9098.js
@@ -0,0 +1,143 @@
+self.__precacheManifest = [
+  {
+    "revision": "dba2d4741d5256aad70a",
+    "url": "/static/js/app.1312b931.chunk.js"
+  },
+  {
+    "revision": "7e4467d9ebe2844f522c",
+    "url": "/static/js/runtime~app.2dbaae62.js"
+  },
+  {
+    "revision": "500c8d58abf94d4ac959",
+    "url": "/static/js/2.8e2f47e9.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "6beba7e6834963f7f171d3bdd075c915",
+    "url": "/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c6aef942e3668158ec29d4adcb2e768f",
+    "url": "/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "872545dde71de3842234bf6afe80c4cb",
+    "url": "/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "5a293a273bee8d740a045d9922b9a9ae",
+    "url": "/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "fb30835e5a0503f95b2f6149c8ee1269",
+    "url": "/manifest.json"
+  },
+  {
+    "url": "/assets/icons/icon_192x192.dd0d8974f196283ab21f6bb647882750.png"
+  },
+  {
+    "url": "/assets/icons/icon_512x512.b370d9dab464db3e849d854262b5d2e5.png"
+  },
+  {
+    "url": "/assets/icons/icon_180x180.ef58ff9a14b1871e5232fada458806e5.png"
+  },
+  {
+    "url": "/assets/splash/icon_1242x2688.46f33d43a6ee160cfb767b7e565d3963.png"
+  },
+  {
+    "url": "/assets/splash/icon_1136x640.24057a53b4b8207ac6128f5a1858a41f.png"
+  },
+  {
+    "url": "/assets/splash/icon_2436x1125.1fd86c39fec0a11f2f6b61d5a7af0f39.png"
+  },
+  {
+    "url": "/assets/splash/icon_828x1792.4ed2cf28de535595662c61587b07aa56.png"
+  },
+  {
+    "url": "/assets/splash/icon_2208x1242.7e9fc39b1c37a328126323bbe8cd404b.png"
+  },
+  {
+    "url": "/assets/splash/icon_1125x2436.b4c95fe40cc182ee41709b5386a65a60.png"
+  },
+  {
+    "url": "/assets/splash/icon_2688x1242.dbbf9209e64340c9d1b78823b8f8abf2.png"
+  },
+  {
+    "url": "/assets/splash/icon_1334x750.ff41747f4f3bb2f9a29c2ee494815852.png"
+  },
+  {
+    "url": "/assets/splash/icon_640x1136.df18c0c33e95b50b62f822d65cd9b909.png"
+  },
+  {
+    "url": "/assets/splash/icon_1242x2208.d8bfb76284287658cab36f271d2a2a4a.png"
+  },
+  {
+    "url": "/assets/splash/icon_750x1334.5d0bcf1c916780f7a20f047bb6d8a30c.png"
+  },
+  {
+    "url": "/assets/splash/icon_1792x828.79a7a4869dc12a14e286c382d496dfd0.png"
+  },
+  {
+    "url": "/assets/splash/icon_2388x1668.de01f138bb18af1e0db00ec6bde434d9.png"
+  },
+  {
+    "url": "/assets/splash/icon_1668x2388.a5a160b95ed25c9e654d2e54edf2908e.png"
+  },
+  {
+    "url": "/assets/splash/icon_2048x2732.2ca94a4a1945d2db226863dbaff1b2de.png"
+  },
+  {
+    "url": "/assets/splash/icon_1668x2224.7df025a8aa1b981a92dffb50d067cd80.png"
+  },
+  {
+    "url": "/assets/splash/icon_2224x1668.28140d990d7a02f90d0d2a40905cb778.png"
+  },
+  {
+    "url": "/assets/splash/icon_2048x1536.3b6c667f7b757be5cd367060e3d67edf.png"
+  },
+  {
+    "url": "/assets/splash/icon_1536x2048.e58c554d58efd43594d17e49e3d4520b.png"
+  },
+  {
+    "url": "/assets/splash/icon_2732x2048.fa6941ee05cda926fb2fed7a5642d92e.png"
+  },
+  {
+    "revision": "41449d343ddd6389ccb569d9a7705dab",
+    "url": "/index.html"
+  }
+];
